d66a836bde0131d611ef1a72b102d6d28721ff47
Julian Welling <julianwellingwork@gmail.com>
2025-03-20 09:27:21 +0000

    fix sample_file_generator

--- config/sample_file_generator.py
+++ config/sample_file_generator.py
@@ -9,20 +9,22 @@ df_samples = pd.DataFrame(columns=ASSEMBLY_TYPES)
 def generate_sample_file(assembly_type, path, output):
     index = 0
     for file_name in listdir(path):
-        df_samples.loc[index, "sample_name"] = file_name.replace(".fna.gz", "")
         if(assembly_type == "none"):
+            df_samples.loc[index, "sample_name"] = file_name.replace(".fna.gz", "")
             df_samples.loc[index, "assembly"] = path + "/" + file_name
         elif(assembly_type == "short" or assembly_type == "hybrid"):
-            file_name_r1 = file_name.replace(".fna.gz", "_R1.fna.gz")
+            df_samples.loc[index, "sample_name"] = file_name.replace(".fastq.gz", "").replace("_R1", "").replace("_R2", "")
+            file_name_r1 = file_name.replace("_R1", "").replace("_R2", "").replace(".fastq.gz", "_R1.fastq.gz")
             df_samples.loc[index, "shortR1"] = path + "/" + file_name_r1
-            file_name_r2 = file_name.replace(".fna.gz", "_R2.fna.gz")
+            file_name_r2 = file_name.replace("_R1", "").replace("_R2", "").replace(".fastq.gz", "_R2.fastq.gz")
             df_samples.loc[index, "shortR2"] = path + "/" + file_name_r2
         if(assembly_type == "long" or assembly_type == "hybrid"):
-            df_samples.loc[index, "long"] = path + "/" + file_name
+            df_samples.loc[index, "sample_name"] = file_name.replace("_R1", "").replace("_R2", "").replace(".fastq.gz", "")
+            df_samples.loc[index, "long"] = path + "/" + file_name.replace("_R1", "").replace("_R2", "")
 
 
         index = index + 1
-        
+    df_samples.drop_duplicates(ignore_index=True, inplace=True)
     df_samples.to_csv(output, sep=',', index=False)
 
 parser = argparse.ArgumentParser(description="Generate samples.csv for all files in folder")
